8f93374bc7d3a3214bef702e28ab6af9e815a25f
Adam Ginzberg <adam@coda.io>
2020-12-18 09:34:42 -0800

    Update particles

--- packs/particles.js
+++ packs/particles.js
@@ -3,46 +3,51 @@ const definition = {
   name: 'Circle Particles',
   dataStructureType: 'TimeSeries', // TODO(David): update this type
   fn: (d3, node, data, width, height) => {
-    var x1 = width / 2,
-        y1 = height / 2,
-        x0 = x1,
-        y0 = y1,
-        i = 0,
-        r = 200,
-        τ = 2 * Math.PI;
+    let x1 = width / 2;
+    let y1 = height / 2;
+    let x0 = x1;
+    let y0 = y1;
+    let i = 0;
+    const r = 200;
+    const τ = 2 * Math.PI;
 
-    function move() {
-      var mouse = d3.mouse(this);
+    function move(d) {
+      const mouse = d3.pointer(d);
       x1 = mouse[0];
       y1 = mouse[1];
-      d3.event.preventDefault();
+      d.preventDefault();
     }
 
-    var context = node.getContext("2d");
-    context.globalCompositeOperation = "lighter";
+    const canvas = d3.select(node);
+    canvas
+      .attr('width', width)
+      .attr('height', height)
+      .on('ontouchstart' in document ? 'touchmove' : 'mousemove', move);
+
+    const context = canvas.node().getContext('2d');
+    context.globalCompositeOperation = 'lighter';
     context.lineWidth = 2;
 
-    d3.timer(function() {
+    d3.timer(() => {
       context.clearRect(0, 0, width, height);
 
-      var z = d3.hsl(++i % 360, 1, .5).rgb(),
-          c = "rgba(" + z.r + "," + z.g + "," + z.b + ",",
-          x = x0 += (x1 - x0) * .1,
-          y = y0 += (y1 - y0) * .1;
+      const z = d3.hsl(++i % 360, 1, 0.5).rgb();
+      const c = 'rgba(' + z.r + ',' + z.g + ',' + z.b + ',';
+      const x = (x0 += (x1 - x0) * 0.1);
+      const y = (y0 += (y1 - y0) * 0.1);
 
-      d3.select({}).transition()
-          .duration(2000)
-          .ease(Math.sqrt)
-          .tween("circle", function() {
-            return function(t) {
-              context.strokeStyle = c + (1 - t) + ")";
-              context.beginPath();
-              context.arc(x, y, r * t, 0, τ);
-              context.stroke();
-            };
-          });
+      d3.select({})
+        .transition()
+        .duration(2000)
+        .ease(Math.sqrt)
+        .tween('circle', () => {
+          return function (t) {
+            context.strokeStyle = c + (1 - t) + ')';
+            context.beginPath();
+            context.arc(x, y, r * t, 0, τ);
+            context.stroke();
+          };
+        });
     });
-
-    // node.on("ontouchstart" in document ? "touchmove" : "mousemove", move);
   },
 };
